45d6131a533459d95872b2e1dc89aa99f93d9f98
Nick McMillen <nickolas.mcmillen@gmail.com>
2022-02-09 17:34:04 +0000

    Working Clock

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('hello')
+
+const display = document.getElementById('clock');
+
+function updateTime() {
+    const date = new Date();
+
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    display.innerText=`${hour} : ${minutes} : ${seconds}`;
+}
+
+setInterval(updateTime, 1000);
