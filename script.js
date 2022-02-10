1161d95ac8b7801b85053fc6dcea9400708ac7ce
Nick McMillen <nickolas.mcmillen@gmail.com>
2022-02-10 18:12:44 +0000

    clock v1.2 24 hour toggle working for one second

--- script.js
+++ script.js
@@ -4,36 +4,53 @@ let audio = new Audio('https://onlineclock.net/audio/options/default.mp3')
 audio.loop = true;
 let alarmTime = null;
 let alarmTimeout = null;
-let timeFormat = new Intl.DateTimeFormat('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-
+let timeFormatTwelve = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',minute: 'numeric', second: 'numeric',
     //hour12: false (use this to change from 12 hour clock to 24)
 });
-let dateFormat = new Intl.DateTimeFormat('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
+let timeFormatTwentyFour = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false,
+})
+
+let dateFormat = new Intl.DateTimeFormat('en-US', {month: 'long', day: 'numeric', year: 'numeric',
 })
 
 function updateDay() {
     let date = new Date();
-
-    //let day = formatTime(date.getDay());
     displayDate.innerText=(dateFormat.format(date));
 }
 
 function updateTime() {
     let date = new Date();
-
-    // let hour = formatTime(date.getHours());
-    // let minutes = formatTime(date.getMinutes());
-    // let seconds = formatTime(date.getSeconds());
-    // //`${hour}:${minutes}:${seconds}`
-
-    displayClock.innerText=(timeFormat.format(date));
+    displayClock.innerText=(timeFormatTwelve.format(date));
 }
+
+function updateTime24() {
+    let date = new Date();
+    displayClock.innerText=(timeFormatTwentyFour.format(date));
+}
+
+// function updateTime() {
+//     let date = new Date();
+
+//     let 12hour = document.getElementById("12hour");
+//     let 24hour = document.getElementById("24hour");
+
+//     if(12hour.checked==true)
+//      displayClock.innerText=(timeFormatTwelve.format(date));
+//     else(24hour.checked==true)
+//      displayClock.innerText=(timeFormatTwentyFour.format(date));
+//}
+
+//     // let hour = formatTime(date.getHours());
+//     // let minutes = formatTime(date.getMinutes());
+//     // let seconds = formatTime(date.getSeconds());
+//     // //`${hour}:${minutes}:${seconds}`
+
+
+
+//     //displayClock.innerText=(timeFormatTwelve.format(date));
+// }
 
 // function formatTime(time) {
 //     if (time < 10) {
@@ -44,7 +61,6 @@ function updateTime() {
 
 function setAlarmTime(value) {
     alarmTime = value;
-    console.log(alarmTime)
 }
 
 function setAlarm() {
